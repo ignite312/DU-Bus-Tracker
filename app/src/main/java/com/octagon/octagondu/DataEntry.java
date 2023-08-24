73cad4647121befd5a1ac65ca5b88acbb661e06d
ignite312 <contact.emonkhan@gmail.com>
2023-08-25 01:43:51 +0600

    Changed

--- app/src/main/java/com/octagon/octagondu/DataEntry.java
+++ app/src/main/java/com/octagon/octagondu/DataEntry.java
@@ -1,10 +1,14 @@
 package com.octagon.octagondu;
 
+import android.annotation.SuppressLint;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +16,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+
 public class DataEntry extends AppCompatActivity {
 
     private Spinner spinnerBusName;
     private Spinner spinnerBusType;
     private TextView textViewBusId;
-    private TextView textViewTime;
+    private ImageView imageViewTime;
     private TextView textViewRouteSt;
-    private TextView textViewRoute;
-
+    private TextView textViewRoute, viewtime;
+    String inputTime;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +37,10 @@ public class DataEntry extends AppCompatActivity {
         spinnerBusName = findViewById(R.id.spinnerBusName);
         spinnerBusType = findViewById(R.id.spinnerBusType);
         textViewBusId = findViewById(R.id.textViewBusId);
-        textViewTime = findViewById(R.id.textViewTime);
+        imageViewTime = findViewById(R.id.setTime);
         textViewRouteSt = findViewById(R.id.textViewRouteSt);
         textViewRoute = findViewById(R.id.textViewRoute);
+        inputTime = "12:00";
 
         // Set click listener for the Submit button
         Button buttonSubmit = findViewById(R.id.buttonSubmit);
@@ -43,7 +51,7 @@ public class DataEntry extends AppCompatActivity {
                 String busName = spinnerBusName.getSelectedItem().toString();
                 String busType = spinnerBusType.getSelectedItem().toString();
                 String busId = textViewBusId.getText().toString();
-                String time = textViewTime.getText().toString();
+                String time = inputTime;
                 String startLocation = textViewRouteSt.getText().toString();
                 String destinationLocation = textViewRoute.getText().toString();
                 if (!busId.isEmpty() && !time.isEmpty() && !startLocation.isEmpty() && !destinationLocation.isEmpty()) {
@@ -61,12 +69,36 @@ public class DataEntry extends AppCompatActivity {
     private void clearForm () {
         // Clear TextViews
         textViewBusId.setText("");
-        textViewTime.setText("");
         textViewRouteSt.setText("");
         textViewRoute.setText("");
 
         // Reset Spinners to default selection (usually the first item)
         spinnerBusName.setSelection(0);
         spinnerBusType.setSelection(0);
+    }
+    public void showTimePickerDialog(View view) {
+        // Get the current time
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        // Create a TimePickerDialog
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        // Update the selected time in the TextView
+                        DataEntry.this.inputTime = String.format("%02d:%02d", hourOfDay, minute);
+//                        viewtime.setText(inputTime);
+                    }
+                },
+                hour,
+                minute,
+                false // Use true if you want 24-hour format, false for 12-hour format
+        );
+
+        // Show the dialog
+        timePickerDialog.show();
     }
 }
--- app/src/main/java/com/octagon/octagondu/MainActivity.java
+++ app/src/main/java/com/octagon/octagondu/MainActivity.java
@@ -1,13 +1,17 @@
 package com.octagon.octagondu;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,6 +19,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -24,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     Button button;
     ImageView imageView;
+    private TextView selectedTimeTextView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -87,5 +95,41 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), SMS.class);
             startActivity(intent);
         });
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        int itemId = menuItem.getItemId();
+
+                        if (itemId == R.id.nav_item1) {
+                            // Handle Item 1 click
+                            showToast("Item 1 clicked");
+                        } else if (itemId == R.id.nav_item2) {
+                            // Handle Item 2 click
+                            showToast("Item 2 clicked");
+                        } else if (itemId == R.id.nav_item3) {
+                            // Handle Item 3 click
+                            showToast("Item 3 clicked");
+                        }
+
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        return true;
+                    }
+                });
+    }
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
