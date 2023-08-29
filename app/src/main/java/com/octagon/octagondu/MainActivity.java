1108d375ee1149d51533fabc8e441dc7a1fd99f1
ignite312 <contact.emonkhan@gmail.com>
2023-08-29 14:19:37 +0600

    Admin for each bus added

--- app/src/main/java/com/octagon/octagondu/MainActivity.java
+++ app/src/main/java/com/octagon/octagondu/MainActivity.java
@@ -1,51 +1,38 @@
 package com.octagon.octagondu;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.telephony.SmsManager;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import android.view.MenuItem;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseReference reference;
-    TextView textView;
-    Button button;
-    ImageView imageView;
-    private TextView selectedTimeTextView;
     private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle actionBarDrawerToggle;
+    Button button;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.DataEntry);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-            }
-        });
+
         button = findViewById(R.id.schedule);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,61 +41,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        button = findViewById(R.id.developer);
-        button.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        // Set up the toggle for the navigation drawer
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Developers.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.home) {
+                    Toast.makeText(MainActivity.this, "Home selected", Toast.LENGTH_SHORT).show();
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
             }
         });
-        imageView = findViewById(R.id.sendEmailIcon);
-        imageView.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), Email.class);
-            startActivity(intent);
-        });
-        imageView = findViewById(R.id.callIcon);
-        imageView.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), SMS.class);
-            startActivity(intent);
-        });
-
-        drawerLayout = findViewById(R.id.drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close
-        );
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                        int itemId = menuItem.getItemId();
-
-                        if (itemId == R.id.nav_item1) {
-                            Intent intent = new Intent(getApplicationContext(), Admin.class);
-                            startActivity(intent);
-                        } else if (itemId == R.id.nav_item2) {
-                            Intent intent = new Intent(getApplicationContext(), Developers.class);
-                            startActivity(intent);
-                        } else if (itemId == R.id.nav_item3) {
-                            Intent intent = new Intent(getApplicationContext(), Email.class);
-                            startActivity(intent);
-                        }else if (itemId == R.id.nav_item4) {
-                            Intent intent = new Intent(getApplicationContext(), SMS.class);
-                            startActivity(intent);
-                        }
-                        drawerLayout.closeDrawer(GravityCompat.START);
-                        return true;
-                    }
-                });
-    }
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
