4af71c3bbd79ddc2c693aef8df25792118192a42
AR-Hridoy-7 <arhridoy2002@gmail.com>
2023-09-20 20:58:17 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/octagon/octagondu/UserProfileActivity.java
+++ app/src/main/java/com/octagon/octagondu/UserProfileActivity.java
@@ -1,8 +1,10 @@
 package com.octagon.octagondu;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        // Find the "Update Profile" button
+        Button updateProfileButton = findViewById(R.id.buttonUpdate);
+
+        // Set an OnClickListener for the button
+        updateProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // When the button is clicked, navigate to the UpdateProfileActivity
+                Intent intent = new Intent(UserProfileActivity.this, UpdateProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
